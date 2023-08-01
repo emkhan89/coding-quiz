a2359013efe8c0bdd8b49437cca48d9d305c11e7
unknown <emkhan89>
2023-08-01 11:16:30 +0100

    added pseudocode to questions.js file for plan of action

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,7 @@
+// 1. make sure questions.js file is above logic.js so it loads first
+
+// 2. use an array with parenthasis inside of it
+
+// 3. use this format - question at the top, choices in the middle and answer at the bottom. 
+    // 3a. use \n to go on to a new line
+    // 3b. work on the logic before the answers. highscores in local storage maybe. 
