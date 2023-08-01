e24a9288d1aeefc7b6d98f7c6b625d80a2aa1939
unknown <emkhan89>
2023-08-01 18:56:09 +0100

    created questions template

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -5,3 +5,43 @@
 // 3. use this format - question at the top, choices in the middle and answer at the bottom. 
     // 3a. use \n to go on to a new line
     // 3b. work on the logic before the answers. highscores in local storage maybe. 
+
+
+var questions [
+    {
+        title: 
+        choices:
+        answer: 
+    }
+
+    {
+        title: 
+        choices:
+        answer: 
+    }
+
+     {
+        title: 
+        choices:
+        answer: 
+    }
+
+     {
+        title: 
+        choices:
+        answer: 
+    }
+
+     {
+        title: 
+        choices:
+        answer: 
+    }
+
+     {
+        title: 
+        choices:
+        answer: 
+    }
+
+]
